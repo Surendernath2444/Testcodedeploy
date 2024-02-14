4c3cb50d6d4c29ee1baba9efc33ac706e6695d8c
Surendernath2444 <153699161+Surendernath2444@users.noreply.github.com>
2024-02-14 14:04:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,9 @@ terraform {
 
 provider "aws" {
   # Configuration options
-  region = "us-east-1"
+aws_access_key_id = AKIAQ3EGUOULI47244H2
+aws_secret_access_key = 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
+region = "us-east-1"
 }
 
 
