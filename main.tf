db5ebf01d2ffeef4c85185bcdc8c0cf249f4c9f3
Surendernath2444 <153699161+Surendernath2444@users.noreply.github.com>
2024-02-21 15:18:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c7217cdde317cfec"
+  ami           = "ami-0440d3b780d96b29d"
   instance_type = "t2.micro"
 
   tags = {
