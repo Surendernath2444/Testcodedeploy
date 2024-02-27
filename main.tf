bfaf1cbcedcaea469ecb216372198a715fbd1901
Surendernath2444 <153699161+Surendernath2444@users.noreply.github.com>
2024-02-27 21:43:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.36.0"
-    }
-  }
-}
-terraform {
-  backend "s3" {
-    bucket = "codebuilds3test"
-    key    = "path/to/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
 provider "aws" {
   region     = "us-east-1"
   access_key = "AKIATCKARQYVEN5V2KPJ"
@@ -20,11 +5,19 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0440d3b780d96b29d"
+  ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
   }
 }
+terraform {
+  backend "s3"{
+    bucket ="testterraformcod"
+    region = "us-east-1"
+    key="terraform.tfstate"
+  }
+}  
+
 
